01ab4e4e6f2788b4d6a624b8a1e711f69e9e3c59
ianhattwick <ian@ianhattwick.com>
2023-03-16 20:19:31 -0400

    create leonard v2

---
+++ leonardv2/mod.ino
@@ -0,0 +1,17 @@
+void modOutput(){
+  static uint16_t val = 0;
+  static uint16_t prev_val = 0;
+  static uint32_t timer = 0;
+  static byte direction = 0;
+  byte rate = 2;
+
+  if(millis()-timer > 1000/127/rate){
+    timer = millis();
+    
+    val = direction == 0 ? val+1 : val-1;
+    direction = val > 125 ? 1 : val < 2 ? 0 : direction;
+
+    if( val != prev_val) MIDImessage( nifty.CC, 1, val );
+    prev_val = val;
+  }
+}
